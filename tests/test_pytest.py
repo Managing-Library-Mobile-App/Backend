3fb1e1358514ce00879a5299b01ff0e77117ae71
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-08-03 20:02:50 +0200

    Creating a sample pytest file to test github actions

---
+++ tests/test_pytest.py
@@ -0,0 +1,21 @@
+class User:
+    def __init__(self, email, password):
+        self.email = email
+        self.hashed_password = self.set_password(password)
+        self.role = 'user'
+
+    def set_password(self, password):
+        # Hash password here
+        return password
+
+
+def test_new_user():
+    """
+    GIVEN
+    WHEN
+    THEN
+    """
+    user = User('patkennedy79@gmail.com', 'FlaskIsAwesome')
+    assert user.email == 'patkennedy79@gmail.com'
+    assert user.hashed_password != 'FlaskIsAwesome'
+    assert user.role == 'user'
