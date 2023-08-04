e941c3e5554ee70e906ffd646a67e0eda9d2b186
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-08-04 11:18:59 +0200

    Added logger

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
+import datetime
+
 from imports import *
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 from routes import available_routes
 from swagger import swagger
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
-
+    """
+    app.logger.debug("debug log info")
+    app.logger.info("Info log information")
+    app.logger.warning("Warning log info")
+    app.logger.error("Error log info")
+    app.logger.critical("Critical log info")
+    """
+    application_start_date = datetime.datetime.now().strftime("%m-%d-%Y %H-%M")
+    logging.basicConfig(
+        filename=f"logs/{application_start_date}.log", level=logging.DEBUG
+    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+    app.logger.info("Started the application")
