869b153ac259798113c747234cc935823ba293e5
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-08-05 20:09:38 +0200

    Added flask debug toolbar

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import datetime
 
 from imports import *
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 api = Api(app)
+app.debug = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = 'SECRET_KEY'
+toolbar = DebugToolbarExtension(app)
 
 from routes import available_routes
+from routes import debug_toolbar
 from swagger import swagger
+
+
+
+
 
 if __name__ == "__main__":
     """
@@ -17,10 +26,10 @@ if __name__ == "__main__":
     app.logger.error("Error log info")
     app.logger.critical("Critical log info")
     """
-    application_start_date = datetime.datetime.now().strftime("%m-%d-%Y %H-%M")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    application_start_date = datetime.datetime.now().strftime("%m-%d-%Y %H-%M-%S")
     logging.basicConfig(
         filename=f"logs/{application_start_date}.log", level=logging.DEBUG
     )
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
     app.logger.info("Started the application")
