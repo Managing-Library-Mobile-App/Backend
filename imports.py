a814b02ed5514ea7f90c1a87e35b86bd4f6bb7db
pvpet <pvpetrus@wp.pl>
2023-10-06 10:11:31 +0200

    removing files

--- imports.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-import logging
-
-from flask import Flask, jsonify
-import flask_sqlalchemy
-from flask_restful import Api, Resource
-from flask_swagger_ui import get_swaggerui_blueprint
