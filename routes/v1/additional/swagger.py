5c852cb5bae1a9eec173372416221a8ad87dfd72
pvpet <pvpetrus@wp.pl>
2023-10-06 10:17:49 +0200

    Added a simple swagger route, file and login swagger description

---
+++ routes/v1/additional/swagger.py
@@ -0,0 +1,17 @@
+import json
+
+from flask import jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
+
+# Dodanie dokumentacji Swagger
+SWAGGER_URL = "/swagger"
+API_URL = "/static/swagger.json"
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, API_URL, config={"app_name": "Library API"}
+)
+
+
+class Swagger:
+    def get(self):
+        with open("swagger/swagger.json", "r") as f:
+            return jsonify(json.load(f))
