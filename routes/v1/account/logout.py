1cd9d7fa5a4174a353d7486c36321c8f38f8ae12
pvpet <pvpetrus@wp.pl>
2023-10-14 20:58:04 +0200

    Added swagger test description

--- routes/v1/account/logout.py
+++ routes/v1/account/logout.py
@@ -1,39 +1,14 @@
 from flask_restful import Resource, reqparse
+from flask_restful_swagger import swagger
 
 
 class Logout(Resource):
+    """Test Description"""
+
+    @swagger.operation(notes="test_note")
     def __init__(self) -> None:
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument(
-            "username",
-            type=str,
-            required=True,
-            location="json",
-        )
         super(Logout, self).__init__()
 
     def post(self) -> dict[str, bool | str]:
-        args = self.reqparse.parse_args()
-        username = args.get("username")
-        # TODO weryfikacja sesji czy faktycznie ktoś z danym username ma aktywną sesję i jest to ta, która jest
-        # w zapytaniu
-        # TODO odkomentować
-        # if False:
-        #     return {
-        #         "logged_out": False,
-        #         "message": "user_not_logged_in",
-        #         "details": "User not logged in (No session)",
-        #     }
-        # TODO weryfikacja w bazie czy taki użytkownik wgl istnieje
-        if username == "Admin-1234":
-            # TODO wyrzucenie sesji po usunięciu użytkownika
-            return {
-                "logged_out": True,
-                "message": "user_deleted",
-                "details": "User deleted successfully",
-            }
-        return {
-            "logged_out": False,
-            "message": "user_does_not_exist",
-            "details": "User does not exist",
-        }
+        pass
