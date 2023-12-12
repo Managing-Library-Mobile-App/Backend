f2ba5192ce2ec8806abf705678853beb50dc7b0d
pvpet <pvpetrus@wp.pl>
2023-12-12 11:56:10 +0100

    Add test mockups

---
+++ tests/functional/login.py
@@ -0,0 +1,46 @@
+def test_positive_login_endpoint_correct_credentials_admin():
+    pass
+
+
+def test_negative_login_endpoint_contains_illegal_char_username():
+    pass
+
+
+def test_negative_login_endpoint_contains_special_char_password():
+    pass
+
+
+def test_negative_login_endpoint_contains_illegal_char_password():
+    pass
+
+
+def test_negative_login_endpoint_contains_uppercase_char_password():
+    pass
+
+
+def test_negative_login_endpoint_length_short_username():
+    pass
+
+
+def test_negative_login_endpoint_length_long_username():
+    pass
+
+
+def test_negative_login_endpoint_length_short_password():
+    pass
+
+
+def test_negative_login_endpoint_length_long_password():
+    pass
+
+
+def test_negative_login_endpoint_already_logged_in():
+    pass
+
+
+def test_negative_login_endpoint_wrong_username_or_password():
+    pass
+
+
+def test_negative_login_endpoint_locked_out_user():
+    pass
