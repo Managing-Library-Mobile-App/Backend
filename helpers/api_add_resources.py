40159466f00c8ad9f32c112a4252deef43f06cbd
pvpet <pvpetrus@wp.pl>
2024-02-12 10:31:58 +0100

    Adding routes for models

--- helpers/api_add_resources.py
+++ helpers/api_add_resources.py
@@ -8,11 +8,18 @@ from routes.v1.account.delete_account import DeleteAccount
 from routes.v1.account.login import Login
 from routes.v1.account.logout import Logout
 from routes.v1.account.register import Register
+from routes.v1.data.other.error import (
+    ErrorGet,
+    ErrorDelete,
+    ErrorAdd,
+)
 
 
 def api_add_resources_v1(api: flask_restful.Api) -> None:
     api_version = "/api/v1/"
     api_without_version = "/api/"
+
+    # additional
     api.add_resource(
         Favicon,
         "/favicon.ico",
@@ -24,6 +31,13 @@ def api_add_resources_v1(api: flask_restful.Api) -> None:
         endpoint="available_routes",
     )
     api.add_resource(
+        AvailableVersions,
+        f"{api_without_version}available_versions",
+        endpoint="available_versions",
+    )
+
+    # account
+    api.add_resource(
         Login,
         f"{api_version}login",
         endpoint="login",
@@ -33,11 +47,7 @@ def api_add_resources_v1(api: flask_restful.Api) -> None:
         f"{api_version}register",
         endpoint="register",
     )
-    api.add_resource(
-        AvailableVersions,
-        f"{api_without_version}available_versions",
-        endpoint="available_versions",
-    )
+
     api.add_resource(
         ChangePassword,
         f"{api_version}change_password",
@@ -53,3 +63,34 @@ def api_add_resources_v1(api: flask_restful.Api) -> None:
         f"{api_version}logout",
         endpoint="logout",
     )
+
+    # author
+
+    # book
+
+    # book announcement
+
+    # error
+    api.add_resource(
+        ErrorAdd,
+        f"{api_version}error_add",
+        endpoint="error_add",
+    )
+    api.add_resource(
+        ErrorGet,
+        f"{api_version}error_get",
+        endpoint="error_get",
+    )
+    api.add_resource(
+        ErrorDelete,
+        f"{api_version}error_delete",
+        endpoint="error_delete",
+    )
+
+    # library
+
+    # opinion
+
+    # user
+
+    # admin
