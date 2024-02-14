bb06291e9b8cf050eb222c8d89a2414e8967b6e6
pvpet <pvpetrus@wp.pl>
2024-02-14 14:16:48 +0100

    removing file logger

--- helpers/init.py
+++ helpers/init.py
@@ -1,11 +1,18 @@
 from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
-from loguru import logger
+
+# from loguru import logger
 from dotenv import load_dotenv
 
 load_dotenv()
 cache = Cache()
 
-logger.add("database_and_logs/logs/logs.log", rotation="10000 seconds")
+# logger.add(
+#     "database_and_logs/logs/logs.log",
+#     format="Log: [{extra[log_id]}]: {time} | {level} | {message} ",
+#     level="INFO",
+#     enqueue=True,
+# )
+
 
 db: SQLAlchemy = SQLAlchemy()
