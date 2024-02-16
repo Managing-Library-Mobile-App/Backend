a9a54574385e578eb86e906b0eeacc890de94f87
pvpet <pvpetrus@wp.pl>
2024-02-16 18:58:14 +0100

    Updating register

--- api/account/register.py
+++ api/account/register.py
@@ -1,19 +1,17 @@
+from __future__ import annotations
+
 import re
 import string
 
+from flask import Response, jsonify
 from flask_restful import reqparse  # type: ignore
 from loguru import logger
-from flasgger import swag_from
 from flask_restful import Resource
+from sqlalchemy import exists
 
-
-class User:
-    active: bool = False
-
-    def __init__(self, username, password, email=None) -> None:
-        self.username = username
-        self.password = password
-        self.email = email
+from models.library import Library
+from models.user import User
+from helpers.init import db
 
 
 def contains_special_char(characters: str) -> bool:
@@ -40,75 +38,80 @@ def authenticate_register_credentials(
 ) -> dict[str, str | None | User]:
     if contains_illegal_char(username):
         return {
-            "user": None,
             "message": "contains_illegal_char_username",
             "details": "Illegal characters in username such as space",
         }
     if not contains_special_char(password):
         return {
-            "user": None,
             "message": "not_contains_special_char_password",
             "details": "No special characters in password. Required at least one",
         }
     if contains_illegal_char(password):
         return {
-            "user": None,
             "message": "contains_illegal_char_password",
             "details": "Illegal characters in password such as space",
         }
     if not contains_uppercase_char(password):
         return {
-            "user": None,
             "message": "not_contains_uppercase_char_password",
             "details": "Password has no uppercase letters. Required at least one.",
         }
     if len(username) < 10:
         return {
-            "user": None,
             "message": "username_too_short",
             "details": "Username should have a minimum of 10 characters",
         }
     if len(username) > 50:
         return {
-            "user": None,
             "message": "username_too_long",
             "details": "Username should have 50 characters max",
         }
     if len(password) < 10:
         return {
-            "user": None,
             "message": "password_too_short",
             "details": "Password should have a minimum of 10 characters",
         }
     if len(password) > 50:
         return {
-            "user": None,
             "message": "password_too_long",
             "details": "Passwrod should have 50 characters max",
         }
     if len(email) > 50:
         return {
-            "user": None,
             "message": "email_too_long",
             "details": "Email should have 50 characters max",
         }
     regex = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b"
     if not re.fullmatch(regex, email):
         return {
-            "user": None,
             "message": "email_of_wrong_format",
             "details": "User already exists",
         }
-    # TODO faktyczna weryfikacja użytkowników, którzy są w bazie
-    if username == "Admin-1234":
+
+    user_already_exists = db.session.query(
+        exists().where(User.username == username, User.email == email)
+    ).scalar()
+
+    if user_already_exists:
         return {
-            "user": None,
             "message": "user_already_exists",
             "details": "User already exists",
         }
-    user = User(username=username, password=password, email=email)
+
+    new_user_library = Library()
+    db.session.add(new_user_library)
+    db.session.commit()
+
+    new_user = User(
+        username=username,
+        password=password,
+        email=email,
+        library_id=new_user_library.id,
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
     return {
-        "user": user,
         "message": "register_successful",
         "details": "Register successful",
     }
@@ -127,34 +130,30 @@ class Register(Resource):
         self.reqparse.add_argument("email", type=str, required=True, location="json")
         super(Register, self).__init__()
 
-    @swag_from("login_swagger.yml")
-    def post(self) -> dict[str, None | str | bool]:
+    def post(self) -> tuple[Response, int]:
         args = self.reqparse.parse_args()
         username = args.get("username")
         password = args.get("password")
         email = args.get("email")
-        registration_output = authenticate_register(username, password, email)
-        if isinstance(registration_output["user"], User):
-            logger.info(
-                f"Zarejestrowano użytkownika {registration_output['user'].username}"
+        registration_output = authenticate_register_credentials(
+            username=username, password=password, email=email
+        )
+        if registration_output["message"] == "register_successful":
+            logger.info(f"Zarejestrowano użytkownika {username}")
+            return (
+                jsonify(
+                    message=str(registration_output["message"]),
+                    details=str(registration_output["details"]),
+                ),
+                200,
             )
-            return {
-                "registered": True,
-                "message": str(registration_output["message"]),
-                "details": str(registration_output["details"]),
-            }
         logger.info(
             f"Nieudana próba rejestracji użytkownika. {registration_output['message']}"
         )
-        return {
-            "registered": False,
-            "message": str(registration_output["message"]),
-            "details": str(registration_output["details"]),
-        }
-
-
-def authenticate_register(username, password, email) -> dict[str, None | str | User]:
-    register_data = authenticate_register_credentials(
-        username=username, password=password, email=email
-    )
-    return register_data
+        return (
+            jsonify(
+                message=str(registration_output["message"]),
+                details=str(registration_output["details"]),
+            ),
+            401,
+        )
