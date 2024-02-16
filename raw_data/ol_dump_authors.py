2f9e019b567921ff5cf8a66d4bff265de50078ec
pvpet <pvpetrus@wp.pl>
2024-02-16 18:58:05 +0100

    getting raw data dumps

--- raw_data/ol_dump_authors.py
+++ raw_data/ol_dump_authors.py
@@ -1,15 +1,57 @@
+import json
+
 import pandas as pd
 
 file_path = "ol_dump_authors_2023-11-30.txt"
 
 
-df = pd.read_csv(file_path, sep="\t", nrows=100)
-
-df.to_excel(file_path + "test.xlsx")
+df = pd.read_csv(
+    file_path,
+    sep="\t",
+    nrows=100,
+    names=["type", "id", "something", "date", "json_data"],
+)
 
 print(df)
 
+df["json_data"] = df["json_data"].apply(json.loads)
+
+# Convert dictionaries into DataFrame columns
+df_normalized = pd.json_normalize(df["json_data"])
+
+print(df_normalized)
+
+# Concatenate the new DataFrame with the original DataFrame, dropping the JSON column
+df = pd.concat([df.drop(columns=["json_data"]), df_normalized], axis=1)
+
+df.drop(
+    columns=[
+        "type",
+        "something",
+        "key",
+        "source_records",
+        "latest_revision",
+        "revision",
+        "type.key",
+        "created.type",
+        "last_modified.type",
+        "personal_name",
+        "birth_date",
+        "alternate_names",
+        "created.value",
+        "modified.value",
+    ],
+    inplace=True,
+)
+
+print(df)
+
+
 # Execution time
+
+df.reset_index(inplace=True, drop=True)
+df.to_csv("ol_dump_authors.csv", index=True, index_label="index")
+df.to_excel(file_path + "test.xlsx")
 
 
 # type - type of record (/type/edition, /type/work etc.) NOT NEEDED
--- raw_data/ol_dump_editions.py
+++ raw_data/ol_dump_editions.py
@@ -1,14 +1,82 @@
+import json
+
 import pandas as pd
 
 file_path = "ol_dump_editions_2023-11-30.txt"
 
-df = pd.read_csv(file_path, sep="\t", nrows=100)
 
-df.to_excel(file_path + "test.xlsx")
+df = pd.read_csv(
+    file_path,
+    sep="\t",
+    nrows=100,
+    names=["type", "id", "something", "date", "json_data"],
+)
 
 print(df)
 
-# TODO przetworzyć dane do innego pliku z tylko potrzebnymi danymi (kolumnami)
+df["json_data"] = df["json_data"].apply(json.loads)
+
+# Convert dictionaries into DataFrame columns
+df_normalized = pd.json_normalize(df["json_data"])
+
+print(df_normalized)
+
+# Concatenate the new DataFrame with the original DataFrame, dropping the JSON column
+df = pd.concat([df.drop(columns=["json_data"]), df_normalized], axis=1)
+
+df.drop(
+    columns=[
+        "type",
+        "something",
+        "physical_format",
+        "number_of_pages",
+        "key",
+        "latest_revision",
+        "oclc_numbers",
+        "works",
+        "revision",
+        "last_modified.type",
+        "last_modified.value",
+        "created.type",
+        "created.value",
+        "type.key",
+        "source_records",
+        "subtitle",
+        "series",
+        "covers",
+        "lc_classifications",
+        "ocaid",
+        "publish_places",
+        "pagination",
+        "dewey_decimal_class",
+        "languages",
+        "lccn",
+        "publish_country",
+        "by_statement",
+        "local_id",
+        "notes.type",
+        "notes.value",
+        "identifiers.goodreads",
+        "contributions",
+        "other_titles",
+        "subject_place",
+        "identifiers.librarything",
+        "subject_time",
+        "uri_descriptions",
+        "url",
+        "uris",
+    ],
+    inplace=True,
+)
+
+print(df)
+
+
+# Execution time
+
+df.reset_index(inplace=True, drop=True)
+df.to_csv("ol_dump_editions.csv", index=True, index_label="index")
+df.to_excel(file_path + "test.xlsx")
 
 
 # type - type of record (/type/edition, /type/work etc.) NOT NEEDED
@@ -16,50 +84,20 @@ print(df)
 # revision - revision number of the record NOT NEEDED
 # last_modified - last modified timestamp NOT NEEDED
 # JSON - the complete record in JSON format:::::::::::::::::::
+# name of type /type/string
+# eastern_order of type /type/boolean NOT NEEDED
+# personal_name of type /type/string
+# enumeration of type /type/string NOT NEEDED
 # title of type /type/string
-# title_prefix of type /type/string NOT NEEDED
-# subtitle of type /type/string NOT NEEDED
-# other_titles[] of type /type/string NOT NEEDED
-# authors[] of type /type/author
-# by_statement of type /type/string NOT NEEDED
-# publish_date of type /type/string
-# copyright_date of type /type/string NOT NEEDED
-# edition_name of type /type/string NOT NEEDED
-# languages[] of type /type/language NOT NEEDED
-# description of type /type/text
-# notes of type /type/text NOT NEEDED
-# genres[] of type /type/string
-# table_of_contents[] of type /type/toc_item NOT NEEDED
-# work_titles[] of type /type/string NOT NEEDED
-# series[] of type /type/string NOT NEEDED
-# physical_dimensions of type /type/string NOT NEEDED
-# physical_format of type /type/string NOT NEEDED
-# number_of_pages of type /type/int NOT NEEDED
-# subjects[] of type /type/string
-# pagination of type /type/string NOT NEEDED
-# lccn[] of type /type/string NOT NEEDED
-# ocaid of type /type/string NOT NEEDED
-# oclc_numbers[] of type /type/string NOT NEEDED
-# isbn_10[] of type /type/string
-# isbn_13[] of type /type/string
-# dewey_decimal_class[] of type /type/string NOT NEEDED
-# lc_classifications[] of type /type/string NOT NEEDED
-# contributions[] of type /type/string
-# publish_places[] of type /type/string NOT NEEDED
-# publish_country of type /type/string NOT NEEDED
-# publishers[] of type /type/string
-# distributors[] of type /type/string NOT NEEDED
-# first_sentence of type /type/text
-# weight of type /type/string
-# location[] of type /type/string NOT NEEDED
-# scan_on_demand of type /type/boolean NOT NEEDED
-# collections[] of type /type/collection NOT NEEDED
+# alternate_names[] of type /type/string NOT NEEDED
 # uris[] of type /type/string NOT NEEDED
-# uri_descriptions[] of type /type/string NOT NEEDED
-# translation_of of type /type/string NOT NEEDED
-# works[] of type /type/work NOT NEEDED
-# source_records[] of type /type/string NOT NEEDED
-# translated_from[] of type /type/language NOT NEEDED
-# scan_records[] of type /type/scan_record NOT NEEDED
-# volumes[] of type /type/volume NOT NEEDED
-# accompanying_material of type /type/string NOT NEEDED
+# bio of type /type/text
+# location of type /type/string
+# birth_date of type /type/string
+# death_date of type /type/string
+# date of type /type/string NOT NEEDED
+# wikipedia of type /type/string NOT NEEDED
+# links[] of type /type/link NOT NEEDED
+# Backreferences
+# books from /type/edition.authors NOT NEEDED?
+# works from /type/work.authors NOT NEEDED?
