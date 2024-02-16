45c43386f4973f214cb5c928b85cab4469763e95
pvpet <pvpetrus@wp.pl>
2024-02-16 18:58:47 +0100

    Updating models

--- models/library.py
+++ models/library.py
@@ -2,45 +2,84 @@ from helpers.init import db
 
 favourite_books = db.Table(
     "favourite_books",
-    db.Column("library_id", db.Integer, db.ForeignKey("library.id"), primary_key=True),
-    db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column(
+        "library_id",
+        db.Integer,
+        db.ForeignKey("library.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    db.Column(
+        "book_id",
+        db.Integer,
+        db.ForeignKey("book.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
 )
 
 bought_books = db.Table(
     "bought_books",
-    db.Column("library_id", db.Integer, db.ForeignKey("library.id"), primary_key=True),
-    db.Column("book_id", db.Integer, db.ForeignKey("book.id"), primary_key=True),
+    db.Column(
+        "library_id",
+        db.Integer,
+        db.ForeignKey("library.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    db.Column(
+        "book_id",
+        db.Integer,
+        db.ForeignKey("book.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
 )
 
 read_books = db.Table(
     "read_books",
-    db.Column("library_id", db.Integer, db.ForeignKey("library.id"), primary_key=True),
-    db.Column("book_id", db.Integer, db.ForeignKey("book.id"), primary_key=True),
+    db.Column(
+        "library_id",
+        db.Integer,
+        db.ForeignKey("library.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    db.Column(
+        "book_id",
+        db.Integer,
+        db.ForeignKey("book.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
 )
 
 
 class Library(db.Model):
     id = db.Column("id", db.Integer, nullable=False, primary_key=True)
-    read_books_count = db.Column(db.Integer, default=0)
-    favourite_books_count = db.Column(db.Integer, default=0)
-    bought_books_count = db.Column(db.Integer, default=0)
+    read_books_count = db.Column(db.Integer, default=0, nullable=True)
+    favourite_books_count = db.Column(db.Integer, default=0, nullable=True)
+    bought_books_count = db.Column(db.Integer, default=0, nullable=True)
     read_books = db.relationship(
         "Book",
         secondary=read_books,
         lazy="subquery",
-        backref=db.backref("read_books", lazy=True),
+        backref=db.backref(
+            "read_books", lazy=True, cascade="all, delete-orphan", single_parent=True
+        ),
     )
     favourite_books = db.relationship(
         "Book",
         secondary=favourite_books,
         lazy="subquery",
-        backref=db.backref("favourite_books", lazy=True),
+        backref=db.backref(
+            "favourite_books",
+            lazy=True,
+            cascade="all, delete-orphan",
+            single_parent=True,
+        ),
     )
     bought_books = db.relationship(
         "Book",
         secondary=bought_books,
         lazy="subquery",
-        backref=db.backref("bought_books", lazy=True),
+        backref=db.backref(
+            "bought_books", lazy=True, cascade="all, delete-orphan", single_parent=True
+        ),
     )
 
     def __init__(self) -> None:
