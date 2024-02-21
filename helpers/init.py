8d6f331d0f2ba63db36e4fc62da7856fe2fb88eb
pvpet <pvpetrus@wp.pl>
2024-02-21 18:16:07 +0100

    add jwt

--- helpers/init.py
+++ helpers/init.py
@@ -1,18 +1,10 @@
 from flask_caching import Cache
+from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
-
-# from loguru import logger
 from dotenv import load_dotenv
 
 load_dotenv()
 cache = Cache()
-
-# logger.add(
-#     "database_and_logs/logs/logs.log",
-#     format="Log: [{extra[log_id]}]: {time} | {level} | {message} ",
-#     level="INFO",
-#     enqueue=True,
-# )
-
-
 db: SQLAlchemy = SQLAlchemy()
+
+jwt = JWTManager()
