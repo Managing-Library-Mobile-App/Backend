ee043f5690f2bf16007159c752556c91f6c894fa
pvpet <pvpetrus@wp.pl>
2024-03-05 21:19:03 +0100

    refactor

--- data/test_data/users.py
+++ data/test_data/users.py
@@ -8,7 +8,7 @@ users: list[dict[str, Any]] = [
     },
     {"username": "Lukasz-1234", "email": "lukasz@gmail.com", "password": "Lukasz-1234"},
     {"username": "Kamila-1234", "email": "kamila@gmail.com", "password": "Kamila-1234"},
-    {"username": "Kamil-1234", "email": "kamin@gmail.com", "password": "Kamil-1234"},
+    {"username": "Kamil-1234", "email": "kamil@gmail.com", "password": "Kamil-1234"},
     {"username": "Marcin-1234", "email": "marcin@gmail.com", "password": "Marcin-1234"},
 ]
 
