1832b0cff98f4eac04ba67b121f301a864929e13
pvpet <pvpetrus@wp.pl>
2024-03-11 21:01:32 +0100

    Correct jwt authentication and add docstring

--- helpers/jwt_auth.py
+++ helpers/jwt_auth.py
@@ -5,17 +5,24 @@ import os
 import jwt
 from flask import jsonify, Response
 from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
+from loguru import logger
 
 from helpers.blocklist import BLOCKED_USER_TOKENS, LOGGED_IN_USER_TOKENS
 
 
 def verify_jwt_token() -> Response | str:
+    """
+    Verify JWT Token for endpoint
+    :returns: response to show if token is invalid or email if valid
+    :rtype: Response | str
+    """
     try:
-        data = verify_jwt_in_request()
+        data = verify_jwt_in_request(optional=True)
+        logger.info(data)
         if data:
             jwt_header, jwt_data = data
         else:
-            raise AttributeError
+            raise ValueError
         token = jwt.encode(
             payload=jwt_data,
             key=os.environ.get("JWT_SECRET_KEY"),
@@ -23,15 +30,18 @@ def verify_jwt_token() -> Response | str:
             headers=jwt_header,
         )
         email = get_jwt_identity()
-        if (
-            email not in LOGGED_IN_USER_TOKENS.keys()
-            or token in BLOCKED_USER_TOKENS[email]
+        logger.info(email)
+        if email in LOGGED_IN_USER_TOKENS.keys() and (
+            email not in BLOCKED_USER_TOKENS.keys()
+            or token not in BLOCKED_USER_TOKENS[email]
         ):
-            raise AttributeError
-    except AttributeError:
+            return email
+        raise ValueError
+    except ValueError:
         return jsonify(
-            password_changed=False,
-            message="user_not_logged_in",
-            details="User not logged in (No session)",
+            msg="Token invalid",
         )
-    return email
+    except Exception:
+        return jsonify(
+            msg="Token invalid",
+        )
