5f9eba6a8b92da7f10a378194ec62619f1aa7da9
pvpet <pvpetrus@wp.pl>
2024-03-24 10:02:04 +0100

    Fix brute force protection

--- api/account/login.py
+++ api/account/login.py
@@ -24,7 +24,7 @@ class InvalidLoginAttemptsCache(object):
 
     @staticmethod
     def _value(
-        lockout_timestamp: float, timestamps: list[float]
+            lockout_timestamp: float, timestamps: list[float]
     ) -> dict[str, list[float] | float]:
         return {
             "lockout_start": lockout_timestamp,
@@ -51,45 +51,43 @@ class InvalidLoginAttemptsCache(object):
 
     @staticmethod
     def invalid_attempt(
-        cache_results: dict, current_datetime: datetime.datetime, usr: str
-    ) -> str | None:
+            cache_results: dict, current_datetime: datetime.datetime, email: str
+    ) -> bool:
         invalid_attempt_timestamps = (
             cache_results["invalid_attempt_timestamps"] if cache_results else []
         )
         invalid_attempt_timestamps = [
             timestamp
             for timestamp in invalid_attempt_timestamps
-            if timestamp
-            > (current_datetime + datetime.timedelta(minutes=-15)).timestamp()
+            if timestamp > (current_datetime + datetime.timedelta(minutes=-15)).timestamp()
         ]
         invalid_attempt_timestamps.append(current_datetime.timestamp())
         if len(invalid_attempt_timestamps) >= 5:
             InvalidLoginAttemptsCache.set(
-                usr, invalid_attempt_timestamps, current_datetime.timestamp()
+                email, invalid_attempt_timestamps, current_datetime.timestamp()
             )
             # TODO po co ten return skoro nic z tym nie robimy
-            return "locked_user_login_attempts"
-        InvalidLoginAttemptsCache.set(usr, invalid_attempt_timestamps)
+            return True
+        InvalidLoginAttemptsCache.set(email, invalid_attempt_timestamps)
+        return False
 
 
 def authenticate_login_credentials(email, password) -> (dict[str, Any], int):
     if not re.fullmatch(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b", email):
-        return EMAIL_WRONG_FORMAT_RESPONSE
+        return create_response(EMAIL_WRONG_FORMAT_RESPONSE)
     if not re.fullmatch(
-        r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_-]{10,50}$",
-        password,
+            r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_-]{10,50}$",
+            password,
     ):
-        return PASSWORD_WRONG_FORMAT_RESPONSE
-    try:
-        User.query.filter_by(email=email, password=password).first()
+        return create_response(PASSWORD_WRONG_FORMAT_RESPONSE)
+    user = User.query.filter_by(email=email, password=password).first()
+    if user:
         if email in LOGGED_IN_USER_TOKENS.keys():
-            return ALREADY_LOGGED_IN_RESPONSE, {"token": LOGGED_IN_USER_TOKENS[email]}
+            return create_response(ALREADY_LOGGED_IN_RESPONSE, {"token": LOGGED_IN_USER_TOKENS[email]})
         token: str = create_access_token(identity=email)
         LOGGED_IN_USER_TOKENS[email] = token
-        return LOGIN_SUCCESSFUL_RESPONSE, {"token": token}
-    except User.DoesNotExist:
-        logger.info("User does not exist")
-    return USER_NOT_LOGGED_IN_RESPONSE
+        return create_response(LOGIN_SUCCESSFUL_RESPONSE, {"token": token})
+    return create_response(USER_NOT_LOGGED_IN_RESPONSE)
 
 
 class Login(Resource):
@@ -103,31 +101,24 @@ class Login(Resource):
         args: dict = self.post_parser.parse_args()
         email: str = args.get("email")
         password: str = args.get("password")
-        return create_response(authenticate_login(email, password))
-
-
-def authenticate_login(email, password) -> dict[str, str | None]:
-    current_datetime: datetime.datetime = datetime.datetime.now(datetime.timezone.utc)
-    cache_results: dict | None = InvalidLoginAttemptsCache.get(email)
-    if cache_results and cache_results.get("lockout_start"):
-        lockout_start_timestamp: float = cache_results.get("lockout_start")
-        lockout_start: datetime.datetime = datetime.datetime.fromtimestamp(
-            lockout_start_timestamp, datetime.timezone.utc
+        current_datetime: datetime.datetime = datetime.datetime.now(datetime.timezone.utc)
+        cache_results: dict | None = InvalidLoginAttemptsCache.get(email)
+        if cache_results and cache_results.get("lockout_start"):
+            lockout_start_timestamp: float = cache_results.get("lockout_start")
+            lockout_start: datetime.datetime = datetime.datetime.fromtimestamp(
+                lockout_start_timestamp, datetime.timezone.utc
+            )
+            locked_out: bool = lockout_start >= (
+                    current_datetime + datetime.timedelta(minutes=-15)
+            )
+            if not locked_out:
+                InvalidLoginAttemptsCache.delete(email)
+            else:
+                logger.warning(f"locked out user: {email}")
+                return create_response(LOCKED_USER_LOGIN_ATTEMPTS_RESPONSE)
+        login_data: (dict[str, Any], int) = authenticate_login_credentials(
+            email=email, password=password
         )
-        locked_out: bool = lockout_start >= (
-            current_datetime + datetime.timedelta(minutes=-15)
-        )
-        if not locked_out:
-            InvalidLoginAttemptsCache.delete(email)
-        else:
-            logger.warning(f"locked out user: {email}")
-            return LOCKED_USER_LOGIN_ATTEMPTS_RESPONSE
-    login_data: (dict[str, Any], int) = authenticate_login_credentials(
-        email=email, password=password
-    )
-    # TODO czy tu nie powinno być if not logged_in? coś tu chyba nie gra
-    if cache_results:
-        InvalidLoginAttemptsCache.invalid_attempt(
-            cache_results, current_datetime, email
-        )
-    return login_data
+        if InvalidLoginAttemptsCache.invalid_attempt(cache_results, current_datetime, email):
+            return create_response(LOCKED_USER_LOGIN_ATTEMPTS_RESPONSE)
+        return login_data
