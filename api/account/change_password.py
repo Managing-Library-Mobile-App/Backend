649d74e843566b38a12cb9f6e73e84b5b4b2256d
pvpet <pvpetrus@wp.pl>
2024-04-03 20:04:43 +0200

    Save changes for initial translations

--- api/account/change_password.py
+++ api/account/change_password.py
@@ -6,6 +6,7 @@ from flask_restful import Resource
 from helpers.init import db
 from helpers.jwt_auth import verify_jwt_token
 from helpers.request_response import RequestParser
+from helpers.translation import translate_dict_to_known
 from models.user import User
 from helpers.request_response import create_response
 from static.responses import PASSWORD_WRONG_FORMAT_RESPONSE, PASSWORD_CHANGED_RESPONSE, \
@@ -17,6 +18,7 @@ class ChangePassword(Resource):
         self.patch_parser: RequestParser = RequestParser()
         self.patch_parser.add_arg("current_password")
         self.patch_parser.add_arg("new_password")
+        self.patch_parser.add_arg("language")
         super(ChangePassword, self).__init__()
 
     def patch(self) -> Response:
@@ -89,9 +91,10 @@ class ChangePassword(Resource):
         args: dict = self.patch_parser.parse_args()
         current_password: str = args.get("current_password")
         new_password: str = args.get("new_password")
+        language: str = args.get("language")
         email: str | None = verify_jwt_token()
         if not email:
-            return create_response(TOKEN_INVALID_RESPONSE)
+            return create_response(translate_dict_to_known(TOKEN_INVALID_RESPONSE, language, ["details"]))
 
         if not re.fullmatch(
             r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_-]{10,50}$",
--- helpers/translation.py
+++ helpers/translation.py
@@ -2,17 +2,21 @@ from googletrans import Translator
 from googletrans.models import Detected
 
 
-def translate_any_to_polish(text_to_translate) -> (str, str):
+def translate_known_to_known(text_to_translate: str, src: str, dest: str) -> str:
+    return Translator().translate(text_to_translate, src=src, dest=dest).text
+
+
+def translate_any_to_known(text_to_translate: str, dest: str) -> str:
     translator = Translator()
     detected_language: Detected = translator.detect(text_to_translate)
     if detected_language.confidence < 0.6:
         raise ValueError("Can't translate, the probability is too low:" + str(detected_language.confidence * 100) + "%")
-    return translator.translate(text_to_translate, src=detected_language.lang, dest='pl').text
+    if detected_language.lang == dest:
+        return text_to_translate
+    return translator.translate(text_to_translate, src=detected_language.lang, dest=dest).text
 
 
-def translate_english_to_polish(text_to_translate):
-    return Translator().translate(text_to_translate, src='en', dest='pl').text
-
-
-def translate_any_to_any(text_to_translate, src, dest):
-    return Translator().translate(text_to_translate, src=src, dest=dest).text
+def translate_dict_to_known(dict_to_translate: dict, dest: str, keys: list[str]) -> dict:
+    for key in keys:
+        dict_to_translate[key] = translate_any_to_known(dict_to_translate[key], dest)
+    return dict_to_translate
