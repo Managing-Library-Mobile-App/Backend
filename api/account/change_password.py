86584bd3ae4fed12d4792a286b97ce2e254ebe31
piotrsrednicki <piotr.srednicki@autologyx.com>
2024-04-05 22:36:31 +0200

    Add nested translations of dicts and lists with exceptions and an example endpoint

--- api/account/change_password.py
+++ api/account/change_password.py
@@ -6,7 +6,6 @@ from flask_restful import Resource
 from helpers.init import db
 from helpers.jwt_auth import verify_jwt_token
 from helpers.request_response import RequestParser
-from helpers.translation import translate_dict_to_known
 from models.user import User
 from helpers.request_response import create_response
 from static.responses import PASSWORD_WRONG_FORMAT_RESPONSE, PASSWORD_CHANGED_RESPONSE, \
@@ -18,7 +17,7 @@ class ChangePassword(Resource):
         self.patch_parser: RequestParser = RequestParser()
         self.patch_parser.add_arg("current_password")
         self.patch_parser.add_arg("new_password")
-        self.patch_parser.add_arg("language")
+        self.patch_parser.add_arg("language", required=False)
         super(ChangePassword, self).__init__()
 
     def patch(self) -> Response:
@@ -94,13 +93,13 @@ class ChangePassword(Resource):
         language: str = args.get("language")
         email: str | None = verify_jwt_token()
         if not email:
-            return create_response(translate_dict_to_known(TOKEN_INVALID_RESPONSE, language, ["details"]))
+            return create_response(TOKEN_INVALID_RESPONSE, language=language)
 
         if not re.fullmatch(
             r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_-]{10,50}$",
             new_password,
         ):
-            return create_response(PASSWORD_WRONG_FORMAT_RESPONSE)
+            return create_response(PASSWORD_WRONG_FORMAT_RESPONSE, language=language)
 
         user: User = User.query.filter_by(
             email=email, password=current_password
@@ -109,5 +108,5 @@ class ChangePassword(Resource):
             user.password = new_password
             db.session.commit()
 
-            return create_response(PASSWORD_CHANGED_RESPONSE)
-        return create_response(PASSWORD_NOT_CHANGED_RESPONSE)
+            return create_response(PASSWORD_CHANGED_RESPONSE, language=language)
+        return create_response(PASSWORD_NOT_CHANGED_RESPONSE, language=language)
