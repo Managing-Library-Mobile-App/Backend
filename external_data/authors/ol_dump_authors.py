5123ab35ec60f732bc0652ee194963e93c767609
piotrsrednicki <piotr.srednicki@autologyx.com>
2024-05-08 23:51:39 +0200

    change csv to json and add fixed filtering and getting columns

--- external_data/authors/ol_dump_authors.py
+++ external_data/authors/ol_dump_authors.py
@@ -4,18 +4,17 @@ import os
 import pandas as pd
 
 read_file_path = os.path.join("../raw_data", "ol_dump_authors_2024-03-31.txt")
-write_file_path = os.path.join("processed_data_authors", "ol_dump_authors.csv")
+write_file_path = os.path.join("processed_data_authors", "ol_dump_authors.json")
 
 if os.path.exists(write_file_path):
     os.remove(write_file_path)
 
-with open(read_file_path, encoding="utf8") as f:
-    print(sum(1 for line in f))
+# 13 million authors
 
 n = 0
 for df in pd.read_csv(
     read_file_path,
-    chunksize=100000,
+    chunksize=1000000,
     sep="\t",
     names=["type", "id", "something", "date", "json_data"],
 ):
@@ -28,15 +27,40 @@ for df in pd.read_csv(
     df_normalized.reset_index(drop=True, inplace=True)
     df = pd.concat([df.drop(columns=["json_data"]), df_normalized], axis="columns")
 
-    df = df[df["id"].notna()]
-    df = df[["id", "name", "photos", "bio.value"]]
-    df = df[df["name"].notna()]
+    filtered_df = pd.DataFrame()
+    columns = [
+        "id",
+        "name",
+        "photos",
+        "bio.value",
+        "death_date",
+        "birth_date",
+        "location",
+        "website",
+    ]
+    for column in columns:
+        if column in df.columns:
+            filtered_df = pd.concat([filtered_df, df[column]], axis="columns")
 
-    df = df.rename(columns={"photos": "picture", "bio.value": "biography"})
+    filtered_fields = [
+        "id",
+        "name",
+    ]
+    for filter_field in filtered_fields:
+        if filter_field in filtered_df.columns:
+            filtered_df = filtered_df[filtered_df[filter_field].notna()]
+
+    filtered_df = filtered_df.rename(
+        columns={"photos": "picture", "bio.value": "biography"}
+    )
     if not os.path.exists(write_file_path):
-        df.to_csv(write_file_path, index=False, mode="w")
+        filtered_df.to_json(
+            write_file_path, lines=True, orient="records", index=False, mode="w"
+        )
     else:
-        df.to_csv(write_file_path, header=False, index=False, mode="a")
+        filtered_df.to_json(
+            write_file_path, lines=True, orient="records", index=False, mode="a"
+        )
 
 with open(write_file_path, encoding="utf8") as f:
     print(sum(1 for line in f))
@@ -47,3 +71,7 @@ with open(write_file_path, encoding="utf8") as f:
 # biography - authors dump 'bio' - nie musi być
 # picture - authors dump 'photos' - nie musi być
 # released_books - edition dump - brane z książek
+# death_date - authors dump 'death_date' - nie musi być
+# birth_date - authors dump 'birth_date' - nie musi być
+# location - authors dump 'location' - nie musi być
+# website - authors dump 'website' - nie musi być
--- external_data/books/ol_dump_editions.py
+++ external_data/books/ol_dump_editions.py
@@ -4,18 +4,17 @@ import os
 import pandas as pd
 
 read_file_path = os.path.join("../raw_data", "ol_dump_editions_2024-03-31.txt")
-write_file_path = os.path.join("processed_data_editions", "ol_dump_editions.csv")
+write_file_path = os.path.join("processed_data_editions", "ol_dump_editions.json")
 
 if os.path.exists(write_file_path):
     os.remove(write_file_path)
 
-with open(read_file_path, encoding="utf8") as f:
-    print(sum(1 for line in f))
+# 45 million editions
 
 n = 0
 for df in pd.read_csv(
     read_file_path,
-    chunksize=10000,
+    chunksize=1000000,
     sep="\t",
     names=["type", "id", "something", "date", "json_data"],
 ):
@@ -28,42 +27,66 @@ for df in pd.read_csv(
     df_normalized.reset_index(drop=True, inplace=True)
     df = pd.concat([df.drop(columns=["json_data"]), df_normalized], axis="columns")
 
-    df = df[
-        [
-            "id",
-            "isbn_10",
-            "isbn_13",
-            "title",
-            "authors",
-            "publishers",
-            "subjects",
-            "publish_date",
-            "works",
-        ]
+    filtered_df = pd.DataFrame()
+    columns = [
+        "id",
+        "isbn_10",
+        "isbn_13",
+        "title",
+        "authors",
+        "publishers",
+        "subjects",
+        "publish_date",
+        "works",
+        "description.value",
+        "languages",
+        "number_of_pages",
     ]
+    for column in columns:
+        if column in df.columns:
+            filtered_df = pd.concat([filtered_df, df[column]], axis="columns")
 
-    df = df[df["id"].notna()]
-    df = df[df["title"].notna()]
-    # df = df.drop(df["isbn_10"].isna() & df["isbn_13"].isna())
-    print(df.shape)
-    df_with_isbn_10 = df[df["isbn_10"].notna()]
-    print(df_with_isbn_10.shape)
-    df_with_isbn_13 = df[df["isbn_13"].notna()]
-    print(df_with_isbn_13.shape)
-    # df = df[df["isbn_10"].notna() or df["isbn_13"].notna()]
-    df = df[df["authors"].notna()]
-    df = df[df["description"].notna()]
-    df = df[df["publish_date"].notna()]
-    df = df[df["language"].notna()]
-    df = df[df["works"].notna()]
+    filtered_fields = [
+        "id",
+        "isbn_10",
+        "title",
+        "authors",
+        "publishers",
+        "description.value",
+        "subjects",
+        "publish_date",
+        "languages",
+        "number_of_pages",
+    ]
+    for filter_field in filtered_fields:
+        if filter_field in filtered_df.columns:
+            filtered_df = filtered_df[filtered_df[filter_field].notna()]
+    filtered_df = filtered_df[filtered_df["id"].notna()]
+    filtered_df.drop(columns=["isbn_10"])
 
-    # isbn_10 and isbn_13 do jednej kolumny i zmienic nazwę i usunąć pozostałe
-
-    df = df.rename(columns={"publish_date": "premiere_date", "bio.value": "biography"})
+    filtered_df = filtered_df.rename(
+        columns={
+            "publish_date": "premiere_date",
+            "description.value": "description",
+            "languages": "language",
+            "subjects": "genres",
+            "publishers": "publishing_house",
+            "isbn_10": "isbn",
+        }
+    )
     if not os.path.exists(write_file_path):
-        df.to_csv(write_file_path, index=False, mode="w")
+        filtered_df.to_json(
+            write_file_path, lines=True, orient="records", index=False, mode="w"
+        )
     else:
-        df.to_csv(write_file_path, header=False, index=False, mode="a")
+        filtered_df.to_json(
+            write_file_path, lines=True, orient="records", index=False, mode="a"
+        )
+    # pd.read_json('output.json',orient='records',lines=True)
+    # filtered_df = filtered_df[
+    #     filtered_df["isbn_10"].notna() | filtered_df["isbn_13"].notna()
+    # ]
+
 
 with open(write_file_path, encoding="utf8") as f:
     print(sum(1 for line in f))
@@ -72,9 +95,10 @@ with open(write_file_path, encoding="utf8") as f:
 # isbn - edition dump - isbn_10 lub isbn_13 (musi być conajmniej 1 z tych 2, a w pierwszej kolejności bierzemy isbn_13)
 # title - edition dump - musi być
 # authors - edition dump - musi być
-# publishing_house - edition dump - nie musi być
+# publishing_house - edition dump - musi być
 # description - edition dump - musi być
-# genres - subjects z edition dump - nie musi być
+# genres - subjects z edition dump - musi być
 # premiere_date - edition dump - musi być
 # language - edition dump - musi być
 # works - musi być
+# number of pages - musi być
